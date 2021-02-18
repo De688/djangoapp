daca1c0d6cb286a86a0d733aeffe264d87f97ee0
christopherdaniel <dedaniel688@gmail.com>
2021-02-18 15:14:47 +0300

    y

--- youtb/settings.py
+++ youtb/settings.py
@@ -107,4 +107,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-django_heroku.settings(locals())STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
